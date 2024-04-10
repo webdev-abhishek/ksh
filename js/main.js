6646e7b92875b0ec5a8225e4b8e4984c80bf3e8c
Abhishek <143903846+webdev-abhishek@users.noreply.github.com>
2024-04-10 19:42:43 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var typed = new Typed(".text", {
+        strings: ["Blind", "Deaf", "Mute"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
